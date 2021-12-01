679c805ef669724b3b57bc402633f68e14407b5f
Marcus Johansson <marcus.johansson@zenseact.com>
2021-12-01 11:33:41 +0100

    Add utils/iterations so that day 1 part 2 works

---
+++ src/utils/iterations.py
@@ -0,0 +1,16 @@
+import collections
+
+
+import collections
+import itertools
+
+# lifted from https://docs.python.org/3/library/itertools.html
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(itertools.islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
