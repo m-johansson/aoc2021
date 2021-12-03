4775bcde22c5f1c690f9f57efa7e6a8ff8b9524a
Marcus Johansson <marcus.johansson@zenseact.com>
2021-12-03 15:31:03 +0100

    Day 3 part 2

--- src/day3.py
+++ src/day3.py
@@ -28,8 +28,21 @@ def get_gamma_and_epsilon(string_generator: Iterable):
     return gamma, epsilon
 
 
+def search_with_bit_condition(input_list: List[str], bit_index:int=0, least:bool=False):
+    if len(input_list) == 1:
+        return input_list[0]
+    leading_ones = [1 if x[bit_index] == "1" else 0 for x in input_list]
+    most_common = 1 if len(leading_ones)/sum(leading_ones) <= 2 else 0
+    if (most_common == 1 and not least) or (most_common == 0 and least):
+        filtered_list = [binary for binary, leading_one in zip(input_list,leading_ones) if leading_one]
+    else:
+        filtered_list = [binary for binary, leading_one in zip(input_list,leading_ones) if not leading_one]
+    return search_with_bit_condition(filtered_list, bit_index=bit_index+1, least=least)
+
+
 if __name__ == "__main__":
     bin_gen = binary_str_generator("inputs/day3.txt")
-    gamma, epsilon = get_gamma_and_epsilon(bin_gen)
-    print(f"{gamma=} {epsilon=}")
-    print(gamma * epsilon)
+    in_ = list(bin_gen)
+    oxygen = int(search_with_bit_condition(in_, bit_index=0, least=False),2)
+    co2_scrubber = int(search_with_bit_condition(in_, bit_index=0, least=True),2)
+    print(f"{oxygen=}, {co2_scrubber=}, {oxygen*co2_scrubber=}")
