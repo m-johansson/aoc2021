0980075c1493a5ca8f7605a759cd12157bc42b0f
Marcus Johansson <marcus.johansson@zenseact.com>
2021-12-11 15:36:33 +0100

    Day 10 black formatting

--- src/day10.py
+++ src/day10.py
@@ -17,15 +17,18 @@ scores_2 = {
 lefters = ("(", "[", "{", "<")
 righters = (")", "]", "}", ">")
 couples = {closer: opener for closer, opener in zip(righters, lefters)}
-class Line:
 
-    def __init__(self, chars:str):
+
+class Line:
+    def __init__(self, chars: str):
         self.corrupt = False
-        self._chars:str = chars.strip()
+        self._chars: str = chars.strip()
         self._score = 0
         self._open_delims = []
 
-    def process(self,):
+    def process(
+        self,
+    ):
         for char in self._chars:
             if char in lefters:
                 self._open_delims.append(char)
@@ -38,25 +41,32 @@ class Line:
                 else:
                     self._open_delims.pop()
 
-    def _set_corrupt(self,):
+    def _set_corrupt(
+        self,
+    ):
         self.corrupt = True
 
-    def set_score(self,):
+    def set_score(
+        self,
+    ):
         if self.corrupt:
             raise ValueError("Corrupt lines have no score")
         s = 0
         for deliminator in reversed(self._open_delims):
             char_score = scores_2[deliminator]
-            s = 5*s + char_score
+            s = 5 * s + char_score
         self._score = s
-    
-    def get_score(self,):
+
+    def get_score(
+        self,
+    ):
         if self.corrupt:
             raise ValueError("Corrupt lines have no scores!")
         return self._score
 
-    def __lt__(self, __ob:object):
+    def __lt__(self, __ob: object):
         return self.get_score() < __ob.get_score()
+
 
 def read_in():
     filepath = pathlib.Path(__file__).parent / "inputs/day10.txt"
@@ -73,7 +83,7 @@ def main():
             l.set_score()
             lines.append(l)
     uncorrupt_lines = sorted([l for l in lines if not l.corrupt])
-    selected = uncorrupt_lines[int(len(uncorrupt_lines)/2)]
+    selected = uncorrupt_lines[int(len(uncorrupt_lines) / 2)]
     print(f"{selected.get_score()}, {selected._chars}")
 
 
